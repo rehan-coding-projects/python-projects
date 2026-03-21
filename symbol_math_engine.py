b4adc8a023805f4928c93e1556b75d1a3325cfc7
rehan-coding-projects <rk01j2011@gmail.com>
2026-03-21 10:12:47 +0530

    Implemented function logic for expression tokenizer, DMAS arithmetic operations, and final evaluation function for future progression

--- symbol_math_engine.py
+++ symbol_math_engine.py
@@ -1,25 +1,69 @@
-list1 = []
-string = input("Enter expression: ")
-num = ""
-print(type(string))
-for i in range(len(string)):
-    char = string[i]
-    if char.isdigit() or char == ".":
-        num = num + char
-    elif char in "+-*/":
-        if num != "":    
-            list1.append(float(num))
-            num = ""
-        list1.append(char)
-    print("num building: ", num)
+def tokenizer(expression):
+    tokens = []
+    current_number = ""
+
+    for char in expression: 
+        
+
+        if char.isdigit() or char == ".":
+            current_number += char
+
+        elif char in "+-*/":
+            if current_number != "":    
+                tokens.append(float(current_number))
+                current_number = ""
+            tokens.append(char)
+        
+    if current_number != "":
+        tokens.append(float(current_number))
+    return tokens
+
+def handle_mul_div(tokens):
+    i = 0
+
+    while i < len(tokens):
+        if tokens[i] == "*" or tokens[i] == "/":
+            left = tokens[i-1]
+            right = tokens[i+1]
+
+            if tokens[i] == "*":
+                result = left * right
+            elif tokens[i] == "/":
+                result = left / right
+
+            tokens[i-1:i+2] = [result]
+            
+            i = 0 #Restart expression iteration
+        else:
+            i+=1
+    return tokens
+
+def handle_add_sub(tokens):
+    j = 0 #Counter
+
+    while j < len(tokens):
+        if tokens[j] == "+" or tokens[j] == "-":
+            left = tokens[j-1]
+            right = tokens[j+1]
+
+            if tokens[j] == "+":
+                result = left + right
+            elif tokens[j] == "-":
+                result = left - right
+            
+            tokens[j-1:j+2] = [result]
+
+            j=0
+        else:
+            j+=1
+    return tokens
     
-if num != "":
-    list1.append(float(num))
+def eval(tokens):
+    tokens = handle_mul_div(tokens)
+    tokens = handle_add_sub(tokens)
+    return tokens[0]
 
-if "*" in list1:
-    g = list1.index("*")
-    product = float(list1[g-1]) * float(list1[g+1])
-    del list1[g-1:g+2]
-    list1.insert(g-1, product)
-
-print(list1)
+expression = input("Enter expression: ")
+tokens = tokenizer(expression)
+result = eval(tokens)
+print(result)
