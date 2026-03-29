73582ee6cc8736530d9ed400f5a3602be1d045ce
rehan-coding-projects <rk01j2011@gmail.com>
2026-03-29 20:14:06 +0530

    Refactor Node class and add arithmetic node classes

--- symbolic-math-engine/ast_node.py
+++ symbolic-math-engine/ast_node.py
@@ -3,7 +3,59 @@ class Node:
         self.value = value
         self.left = left
         self.right = right
+    
+    def __repr__(self):
+        if self.left and self.right:
+            return f"({self.left} {self.value} {self.right})"
+        return str(self.value)
 
+class NumNode:
+    def __init__(self, value):
+        self.value = value
+    def evaluate(self):
+        return self.value
+    
+class AddNode:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+    def evaluate(self):
+        return self.left.evaluate() + self.right.evaluate()
 
+class SubNode:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+    def evaluate(self):
+        return self.left.evaluate() - self.right.evaluate()
+
+class MultNode:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+    def evaluate(self):
+        return self.left.evaluate() * self.right.evaluate()
+
+class DivNode: 
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+    def evaluate(self):
+        return self.left.evaluate() / self.right.evaluate()
+
+class ExpNode:
+    def __init__(self, base, expo):
+        self.base = base
+        self.expo = expo
+    def evaluate(self):
+        return self.base.evaluate() ** self.expo.evaluate()
+    
+class UnaryMinusNode:
+    def __init__(self, opr):
+        self.opr = opr
+    def evaluate(self):
+        return -self.opr.evaluate()
+
+    
 
 #Node("*", Node("+", Node(3), Node(4)), Node(2))
