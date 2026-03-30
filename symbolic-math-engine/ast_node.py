5fec924b42137924c5a09ca6f5ef05e9baf79741
rehan-coding-projects <rk01j2011@gmail.com>
2026-03-30 20:25:28 +0530

    Enhance AST nodes with context evaluation and variables
    Added context handling to evaluate methods and introduced VariableNode class for symbolic operations.

--- symbolic-math-engine/ast_node.py
+++ symbolic-math-engine/ast_node.py
@@ -12,49 +12,112 @@ class Node:
 class NumNode:
     def __init__(self, value):
         self.value = value
-    def evaluate(self):
+    def evaluate(self, context={}):
         return self.value
+    def __repr__(self):
+        return str(self.value)
     
 class AddNode:
     def __init__(self, left, right):
         self.left = left
         self.right = right
-    def evaluate(self):
-        return self.left.evaluate() + self.right.evaluate()
+    def evaluate(self, context={}):
+        left_val = self.left.evaluate(context)
+        right_val = self.right.evaluate(context)
+
+        if isinstance(left_val, (int,float)) and isinstance(right_val, (int, float)):
+            return left_val + right_val
+        
+        return AddNode(left_val, right_val)
+    
+    def __repr__(self):
+        return(f"({self.left} + {self.right})")
 
 class SubNode:
     def __init__(self, left, right):
         self.left = left
         self.right = right
-    def evaluate(self):
-        return self.left.evaluate() - self.right.evaluate()
+    def evaluate(self, context={}):
+        left_val = self.left.evaluate(context)
+        right_val = self.right.evaluate(context)
+
+        if isinstance(left_val, (int,float)) and isinstance(right_val, (int, float)):
+            return left_val - right_val
+        
+        return SubNode(left_val, right_val)
+    def __repr__(self):
+        return(f"({self.left} - {self.right})")
 
 class MultNode:
     def __init__(self, left, right):
+        print("Multiply:", left, right)
         self.left = left
         self.right = right
-    def evaluate(self):
-        return self.left.evaluate() * self.right.evaluate()
+    def evaluate(self, context={}):
+        left_val = self.left.evaluate(context)
+        right_val = self.right.evaluate(context)
+
+        if isinstance(left_val, (int,float)) and isinstance(right_val, (int, float)):
+            return left_val * right_val
+        
+        return MultNode(left_val, right_val)
+    def __repr__(self):
+        return(f"({self.left} * {self.right})")
 
 class DivNode: 
     def __init__(self, left, right):
         self.left = left
         self.right = right
-    def evaluate(self):
-        return self.left.evaluate() / self.right.evaluate()
+    def evaluate(self, context={}):
+        left_val = self.left.evaluate(context)
+        right_val = self.right.evaluate(context)
+
+        if isinstance(left_val, (int,float)) and isinstance(right_val, (int, float)):
+            return left_val / right_val
+        
+        return DivNode(left_val, right_val)
+    def __repr__(self):
+        return(f"({self.left} / {self.right})")
 
 class ExpNode:
     def __init__(self, base, expo):
         self.base = base
         self.expo = expo
-    def evaluate(self):
-        return self.base.evaluate() ** self.expo.evaluate()
+    def evaluate(self, context={}):
+        base_val = self.base.evaluate(context)
+        expo_val = self.expo.evaluate(context)
+
+        if isinstance(base_val, (int, float)) and isinstance(expo_val, (int, float)):
+            return base_val ** expo_val
+        return ExpNode(base_val, expo_val)
+    
+    def __repr__(self):
+        return(f"({self.base} ^ {self.expo})")
     
 class UnaryMinusNode:
     def __init__(self, opr):
         self.opr = opr
-    def evaluate(self):
-        return -self.opr.evaluate()
+    def evaluate(self, context={}):
+        val = self.opr.evaluate(context)
+
+        if isinstance(val, (int, float)):
+            return -val
+        return UnaryMinusNode(val)
+    
+    def __repr__(self):
+        return f"-({self.opr})"
+
+class VariableNode: #Handles variables and symbolic operations
+    def __init__(self, name):
+        self.name = name
+    
+    def evaluate(self, context={}):
+        if self.name in context:
+            return context[self.name]
+        return self
+    
+    def __repr__(self):
+        return self.name
 
     
 
