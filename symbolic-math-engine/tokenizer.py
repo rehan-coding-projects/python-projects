007a2266f8c80ee19bca5216a06526d8e94aed5c
rehan-coding-projects <rk01j2011@gmail.com>
2026-03-30 20:25:50 +0530

    Refactor tokenizer to improve number parsing and error handling
    Refactor tokenizer function to handle multi-digit numbers and decimals correctly, and add error handling for invalid syntax.

--- symbolic-math-engine/tokenizer.py
+++ symbolic-math-engine/tokenizer.py
@@ -1,19 +1,36 @@
-def tokenizer(expression):
+def tokenizer(expr):
     tokens = []
-    current_number = ""
-
-    for char in expression: 
-        
+    i = 0
+    while i<len(expr):
+        char = expr[i]
 
         if char.isdigit() or char == ".":
-            current_number += char
+            num = char
+            dot_count = 1 if char =="." else 0
+            i+=1
 
-        elif char in "+-*/^()":
-            if current_number != "":    
-                tokens.append(float(current_number))
-                current_number = ""
-            tokens.append(char)
+            while i < len(expr) and (expr[i].isdigit() or expr[i] == "."):
+                if expr[i] == ".":
+                    dot_count +=1
+                    if dot_count > 1:
+                        raise Exception("Expected 1 decimal only")
+                num += expr[i]
+                i+=1
+            tokens.append(int(num))
         
-    if current_number != "":
-        tokens.append(float(current_number))
+        elif char.isalpha():
+            tokens.append(char)
+            i+=1
+        
+        elif char in "+-*/^()":
+            tokens.append(char)
+            i+=1
+        
+        elif char == " ":
+            i+=1
+
+        else: 
+            raise Exception("Invalid syntax")
+        
     return tokens
+
