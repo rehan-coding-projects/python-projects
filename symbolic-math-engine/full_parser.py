26baa90515777b6fa76c2e2d60bb70342b0782f0
rehan-coding-projects <rk01j2011@gmail.com>
2026-04-30 12:40:30 +0530

    Refactor parser to handle function parsing

--- symbolic-math-engine/full_parser.py
+++ symbolic-math-engine/full_parser.py
@@ -7,9 +7,16 @@ class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
         self.pos = 0
+        self.in_function = False
 
     def parse_factor(self):
+        
+        if self.pos >= len(self.tokens):
+
+            raise Exception("Unexpected end of input")
         current = self.tokens[self.pos]
+
+        
         if isinstance(current, (int, float)):
             node = NumNode(current)
             self.pos += 1
@@ -21,11 +28,15 @@ class Parser:
         elif current == "(":
             self.pos += 1
             node = self.parse_expression()
+
             if self.pos >= len(self.tokens) or self.tokens[self.pos] != ")":
                 raise Exception("Expected )")
             self.pos += 1
             return node
-        elif current.isalpha():
+        elif current.isalpha() and isinstance(current, str):
+            if self.pos + 1 < len(self.tokens) and self.tokens[self.pos + 1] == "(":
+                return self.parse_function()
+            
             self.pos +=1
             return VariableNode(current)
         else:
@@ -41,7 +52,7 @@ class Parser:
         return left 
     def parse_term(self):
         node = self.parse_power()
-
+        print(f"[Term] pos={self.pos}, token={self.tokens[self.pos:]}")
         while self.pos < len(self.tokens):
             cur = self.tokens[self.pos]
 
@@ -53,8 +64,7 @@ class Parser:
                     node = MultNode(node, right)
                 elif op == "/":
                     node = DivNode(node, right)
-            elif isinstance(cur, (int,float)) or cur.isalpha() or cur =="(":
-
+            elif cur =="(" or isinstance(cur, (int,float)) or isinstance(cur, str) and cur.isalpha() and not (self.pos + 1 < len(self.tokens) and self.tokens[self.pos+1] == "<"):
                 right = self.parse_factor()
                 node = MultNode(node, right)
             else:
@@ -63,7 +73,7 @@ class Parser:
     def parse_expression(self):
         node = self.parse_term()
 
-        while self.pos < len(self.tokens) and self.tokens[self.pos] in ("+","-"):
+        while self.pos < len(self.tokens) and self.tokens[self.pos] not in (")",):
             op = self.tokens[self.pos]
             self.pos += 1
             right = self.parse_term()
@@ -72,3 +82,20 @@ class Parser:
             elif op == "-":
                 node = SubNode(node, right)
         return node
+    def parse_function(self):
+        name = self.tokens[self.pos]
+        self.pos +=1
+
+        if self.pos>= len(self.tokens) or self.tokens[self.pos] != "(":
+            raise Exception("Expected (")
+        self.pos+=1
+
+        self.in_function = True
+        arg = self.parse_expression()
+        self.in_function = False
+        if self.pos>= len(self.tokens) or self.tokens[self.pos] != ")":
+            raise Exception("Expected )")
+        
+        self.pos+=1
+        return FunctionNode(name, arg)
+
