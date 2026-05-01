af44b956dd358680004964f1856f188f3d5b93a0
rehan-coding-projects <rk01j2011@gmail.com>
2026-05-01 12:07:03 +0530

    Refactor evaluation logic and add equation processing
    Refactor evaluate function into process function and add support for solving equations.

--- symbolic-math-engine/main.py
+++ symbolic-math-engine/main.py
@@ -1,17 +1,36 @@
 from tokenizer import tokenizer
 from full_parser import Parser
 import math
+from astsolver import *
+from engine import evaluate
+from eqsolver import *
 
-
-def evaluate(expr, context=None):
+def process(expr, context=None):
     if context is None:
-        context={}
+        context = {
+            'x':0,
+            'y':0,
+            'pi' : math.pi,
+            'e' : math.e
+        }
+
+    if "=" in expr:
+        left_str, right_str = expr.split("=")
+
+        _, left_ast = evaluate(left_str, context)
+        _, right_ast = evaluate(right_str, context)
+
+        print(f"DEBUG AST STRUCTURE: {left_ast}")
+        ast = SubNode(left_ast, right_ast)
     
-    tokens = tokenizer(expr)
-    parser = Parser(tokens)
-    ast = parser.parse_expression()
+        a,b,c = extract_quadratic(ast)
+        
+        if a != 0:
+            return solve_quadratic(a,b,c)
+        else:
 
-    result = ast.evaluate(context)
-    return result
+            return solve_linear(b,c)
+        
+    value, ast = evaluate(expr, context)
+    return value
 
-[]
